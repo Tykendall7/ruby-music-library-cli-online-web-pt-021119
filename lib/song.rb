f2216432b15a125b4db5c027932b1c13c9cdec32
Tymeckia Kendall <tykend@gmail.com>
2019-04-02 00:44:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,7 @@ class Song
     self.all.find{|s| s.name == song_name}
   end
   
-  def self.find_or_create_by_name (name)
+  def self.find_or_create_by_name (song_name)
     self.find_by_name(song_name) || self.create_by_name(name)
   end
   
