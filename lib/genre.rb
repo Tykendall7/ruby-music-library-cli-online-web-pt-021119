4793433d13f45f61244242324e2ac312da9b4fea
Tymeckia Kendall <tykend@gmail.com>
2019-04-02 00:34:23 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,35 @@
+class Genre
+  attr_accessor :name, :songs, :artists
+  
+  @@all = []
+  
+  def initialize (name)
+    @name = name
+    @@all << self
+    @songs = []
+  end
+  
+  def artists
+    songs.collect { |s| s.artist}.uniq
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    self.all.clear
+  end
+  
+  def save
+    @@all << self.class.all
+  end
+  
+  def self.create(genre)
+   genre= self.new (genre)
+   @genre= genre
+   @genre
+ end
+  
+  
+end
---
+++ lib/song.rb
@@ -0,0 +1,42 @@
+class Song
+  attr_accessor :name, :song, :artist, :genre
+ 
+  
+  @@all = []
+  
+  def initialize (name, artist = nil, genre = nil)
+    @name = name
+    @@all << self
+    self.artist=(artist) if artist
+   self.genre=(genre) if genre
+  end
+  
+  def artist=(artist)
+    @artist = artist
+   artist.add_song(self) unless artist.nil?
+  end
+  def genre=(genre)
+    @genre= genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    self.all.clear
+  end
+  
+  def save
+    @@all << self.class.all
+  end
+  
+  def self.create(song)
+   song= self.new (song)
+   @song= song
+   @song
+ end
+  
+
+  
+end
