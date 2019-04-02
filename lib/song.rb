20d855ace598e8950617fa8299113ee6ff8263c2
Tymeckia Kendall <tykend@gmail.com>
2019-04-02 01:24:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,18 @@ class Song
     end
   end
   
-  def self.new_from_filename(song_name)
-    
+    def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name, song_name, genre_name = parts[0], parts[1], parts[2].gsub(".mp3","")
+
+     artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+
+     self.new(song_name,artist,genre)
   end
+
+   def self.create_from_filename(filename)
+    self.new_from_filename(filename).tap{ |s| s.save}
+  end
+  
 end
