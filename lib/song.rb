deade6d08fdfc74897f797928a4d6aaf19f89948
Tymeckia Kendall <tykend@gmail.com>
2019-04-02 00:42:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,7 @@ class Song
  end
   
   def self.find_by_name(song_name)
-    self.all.find{|s| s.name == name}
+    self.all.find{|s| s.name == song_name}
   end
   
   def self.find_or_create_by_name (name)
